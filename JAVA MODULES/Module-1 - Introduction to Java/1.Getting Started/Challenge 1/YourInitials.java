3f11c411acb41de0a9eab6939ac2616a35a410bc
Baibhab <baibhabnayak.31@gmail.com>
2025-01-28 11:09:47 +0530

    Challenge 1 solved

--- JAVA MODULES/Module-1 - Introduction to Java/1.Getting Started/Challenge 1/YourInitials.java
+++ JAVA MODULES/Module-1 - Introduction to Java/1.Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,13 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+        System.out.println("BBBB   N   N");
+        System.out.println("B   B  NN  N");
+        System.out.println("B   B  N N N");
+        System.out.println("BBBB   N  NN");
+        System.out.println("B   B  N   N");
+        System.out.println("B   B  N   N");
+        System.out.println("BBBB   N   N");
+
     }
 }
