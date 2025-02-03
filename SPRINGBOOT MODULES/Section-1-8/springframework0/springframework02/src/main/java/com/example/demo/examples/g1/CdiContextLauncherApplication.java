5b27062506a8949e26540d365cd61f7af22d7611
Baibhab <baibhabnayak.31@gmail.com>
2025-02-03 11:09:52 +0530

    SPRINGBOOT section 9 completed

---
+++ SPRINGBOOT MODULES/Section-1-8/springframework0/springframework02/src/main/java/com/example/demo/examples/g1/CdiContextLauncherApplication.java
@@ -0,0 +1,55 @@
+package com.example.demo.examples.g1;
+
+import java.util.Arrays;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.inject.Inject;
+import jakarta.inject.Named;
+
+
+@Named
+class BusinessService {
+    private DataService dataService;
+
+    @Inject
+    public void setDataService(DataService dataService) {
+        System.out.println("Setter Injection");
+        this.dataService = dataService;
+    }
+
+
+    public DataService getDataService() {
+        return dataService;
+    }
+
+
+
+}
+
+@Named
+class DataService {
+
+}
+
+@Configuration
+@ComponentScan
+public class CdiContextLauncherApplication {
+
+    public static void main(String[] args) {
+
+        try (var context =
+                     new AnnotationConfigApplicationContext
+                             (CdiContextLauncherApplication.class)) {
+
+            Arrays.stream(context.getBeanDefinitionNames())
+                    .forEach(System.out::println);
+
+            System.out.println(context.getBean(BusinessService.class)
+                    .getDataService());
+
+        }
+    }
+}
